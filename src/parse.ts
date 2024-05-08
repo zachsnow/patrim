37e8090ac5c888c1f242230f6dbb48b4a2421f80
Zach Snow <z@zachsnow.com>
2024-05-08 07:11:48 -0700

    Note source; simplify to match what we care about for now. Maybe we should use unraw?

--- src/parse.ts
+++ src/parse.ts
@@ -209,46 +209,40 @@ export const lex = (input: string): Token[] => {
 };
 
 /**
- * "Unescapes" escape sequences in `input`, so that e.g. `"\\n"` becomes `"\n"`.
+ * Interpret escape sequences in `input`, so that e.g. `"\\n"` becomes `"\n"`.
+ *
+ * See: https://stackoverflow.com/a/57330383/25381 by T.J. Crowder.
  *
  * @param input
  * @returns the input string with escapes interpreted.
  */
 const interpretEscapes = (input: string): string => {
-  return input.replace(
-    /\\[0-9]|\\['"\bfnrtv]|\\x[0-9a-f]{2}|\\u[0-9a-f]{4}|\\u\{[0-9a-f]+\}|\\./gi,
-    (match) => {
-      switch (match[1]) {
-        case "'":
-        case '"':
-        case "\\":
-          return match[1];
-        case "b":
-          return "\b";
-        case "f":
-          return "\f";
-        case "n":
-          return "\n";
-        case "r":
-          return "\r";
-        case "t":
-          return "\t";
-        case "v":
-          return "\v";
-        case "u":
-          if (match[2] === "{") {
-            return String.fromCodePoint(parseInt(match.substring(3), 16));
-          }
-          return String.fromCharCode(parseInt(match.substring(2), 16));
-        case "x":
-          return String.fromCharCode(parseInt(match.substring(2), 16));
-        case "0":
-          return "\0";
-        default: // E.g., "\q" === "q"
-          return match.substring(1);
-      }
-    },
-  );
+  return input.replace(/\\["\bfnrtv0]/gi, (match) => {
+    switch (match[1]) {
+      case "'":
+      case '"':
+      case "\\":
+        return match[1];
+      case "b":
+        return "\b";
+      case "f":
+        return "\f";
+      case "n":
+        return "\n";
+      case "r":
+        return "\r";
+      case "t":
+        return "\t";
+      case "v":
+        return "\v";
+      case "0":
+        return "\0";
+      default:
+        // Should never match unless you change the regex and fail to update
+        // the cases.
+        return match.substring(1);
+    }
+  });
 };
 
 /**
